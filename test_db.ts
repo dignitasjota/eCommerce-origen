0c8c1bfd68c0ebb8d6a3de26ae53cc9ce38e8931
Jota <dignitasjota@gmail.com>
2026-03-03 19:01:43 +0100

    chore: remove test/debug files to keep root directory clean for Plesk builds

--- test_db.ts
+++
@@ -1,6 +0,0 @@
-import prisma from './src/lib/db';
-async function run() {
-    const pages = await prisma.page.findMany();
-    console.log("PAGES ARE:", pages);
-}
-run();
--- test_db2.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-    let rawPage = await prisma.page.findMany({});
-    console.log("pages: ", rawPage);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
